3f3a92dc1776a374407311d7e1109fb1c0d171f4
sheraz <sherz12r@gmail.com>
2026-06-02 21:56:09 +0400

    location added as UAE inshallah

--- config/search.py
+++ config/search.py
@@ -22,7 +22,7 @@ version:    26.01.20.5.08
 search_terms = ["Senior Web Developer", "laravel developer", "Full Stack Developer", "Web Developer", "Automation Engineer"]
 
 # Search location, this will be filled in "City, state, or zip code" search box. If left empty as "", tool will not fill it.
-search_location = "Abu Dhabi, Abu Dhabi Emirate, United Arab Emirates"               # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
+search_location = "United Arab Emirates"               # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
 
 # After how many number of applications in current search should the bot switch to next search? 
 switch_number = 30                 # Only numbers greater than 0... Don't put in quotes
