b46110a7da2e183ddfc7eef3d4d8f4426709e1ff
sheraz <sherz12r@gmail.com>
2026-06-02 21:54:46 +0400

    automation engineer added in job search inshallah

--- config/search.py
+++ config/search.py
@@ -19,7 +19,7 @@ version:    26.01.20.5.08
 
 # These Sentences are Searched in LinkedIn
 # Enter your search terms inside '[ ]' with quotes ' "searching title" ' for each search followed by comma ', ' Eg: ["Software Engineer", "Software Developer", "Selenium Developer"]
-search_terms = ["Senior Web Developer", "laravel developer", "Full Stack Developer", "Web Developer"]
+search_terms = ["Senior Web Developer", "laravel developer", "Full Stack Developer", "Web Developer", "Automation Engineer"]
 
 # Search location, this will be filled in "City, state, or zip code" search box. If left empty as "", tool will not fill it.
 search_location = "Abu Dhabi, Abu Dhabi Emirate, United Arab Emirates"               # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
